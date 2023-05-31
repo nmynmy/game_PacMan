ce51d22f950f922c69b973acab7e1c3141ac48e4
Grigory <chahov03@gmail.com>
2023-05-31 05:00:46 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kakas/CLionProjects/untitled1/ConvertSketch.cpp" "CMakeFiles/untitled1.dir/ConvertSketch.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/ConvertSketch.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/DrawMap.cpp" "CMakeFiles/untitled1.dir/DrawMap.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/DrawMap.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/DrawText.cpp" "CMakeFiles/untitled1.dir/DrawText.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/DrawText.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/Ghost.cpp" "CMakeFiles/untitled1.dir/Ghost.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/Ghost.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/GhostManager.cpp" "CMakeFiles/untitled1.dir/GhostManager.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/GhostManager.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/MapCollision.cpp" "CMakeFiles/untitled1.dir/MapCollision.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/MapCollision.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/Pacman.cpp" "CMakeFiles/untitled1.dir/Pacman.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/Pacman.cpp.obj.d"
+  "C:/Users/kakas/CLionProjects/untitled1/main.cpp" "CMakeFiles/untitled1.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/ConvertSketch.cpp.obj"
+  "CMakeFiles/untitled1.dir/ConvertSketch.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/DrawMap.cpp.obj"
+  "CMakeFiles/untitled1.dir/DrawMap.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/DrawText.cpp.obj"
+  "CMakeFiles/untitled1.dir/DrawText.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/Ghost.cpp.obj"
+  "CMakeFiles/untitled1.dir/Ghost.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/GhostManager.cpp.obj"
+  "CMakeFiles/untitled1.dir/GhostManager.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/MapCollision.cpp.obj"
+  "CMakeFiles/untitled1.dir/MapCollision.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/Pacman.cpp.obj"
+  "CMakeFiles/untitled1.dir/Pacman.cpp.obj.d"
+  "CMakeFiles/untitled1.dir/main.cpp.obj"
+  "CMakeFiles/untitled1.dir/main.cpp.obj.d"
+  "libuntitled1.dll.a"
+  "untitled1.exe"
+  "untitled1.exe.manifest"
+  "untitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
